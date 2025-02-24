97ebdc521928f2d436af989dee824fe333781269
usman siddiqi <usmansiddiqi2000@gmail.com>
2025-02-24 12:22:55 +0530

    chnaged backgrounds

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -4,13 +4,17 @@ import Footer from '../components/Footer'
 import Heroimg2 from '../components/Heroimg2'
 import Form from '../components/Form'
 
-const   Contact = () => {
+const Contact = () => {
   return (
-    <div>
-   
-     <Heroimg2 heading={"CONTACT"} text = {"Lets have a chat"}/>
-     <Form/>
-     
+    <div style={{ 
+        backgroundImage: "url('../assets/contactbg.jpg')",  // Add your background image here
+        backgroundSize: "cover", // Ensures the image covers the entire component
+        backgroundPosition: "center", // Centers the background image
+        backgroundRepeat: "no-repeat", // Ensures the background image doesn't repeat
+        minHeight: "100vh", // Makes sure the component covers the full height of the viewport
+    }}>
+      <Heroimg2 heading={"CONTACT"} text={"Lets have a chat"}/>
+      <Form/>
     </div>
   )
 }
